cbc922cb8d1cf0dddf01a0ad6a77f0ac1dc7f574
mahesh1105 <maheshsaini1105@gmail.com>
2025-01-22 19:55:00 +0530

    Project: Increment or Decrement counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+/*
+  Hooks allow us to "hook" into React features such as state and lifecycle methods
+  You must import Hooks from react: import { useState } from 'react'
+
+  Here we are using the useState Hook to keep track of the application state.
+  State generally refers to application data or properties that need to be tracked.
+
+  There are 3 rules for hooks:
+  ----------------------------
+  - Hooks can only be called inside React function components.
+  - Hooks can only be called at the top level of a component.
+  - Hooks cannot be conditional
+
+  We initialize our state by calling useState in our function component.
+  ** useState accepts an initial state and returns two values:
+  -----------------------
+  - The current state.
+  - A function that updates the state.
+
+  let [counter, setCounter] = useState(5);
+  
+  Here, Initial Value for the state is 5 and state name is counter that is maintained across the function component
+  setCounter function is used to update the state
+
+  - The useState Hook can be used to keep track of strings, numbers, booleans, arrays, objects, and any combination of these!
+
+  1. We could create multiple state Hooks to track individual values.
+  const [brand, setBrand] = useState("Ford");
+  const [model, setModel] = useState("Mustang");
+  const [year, setYear] = useState("1964");
+  const [color, setColor] = useState("red");
+
+  2. we can just use one state and include an object instead!
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+  });
+
+  If state gets updated then entire state will be overwritten.
+  So, if we want to change the any particular property of the object, like - color
+  we only call setCar({color: "blue"}), this would remove the brand, model, and year from our state.
+  In that case, we need to use the spread operator
+*/
+
+/*
+  The main use of hooks comes, when you want to reflect the updated variable over multiple places in UI
+  To Do So, without using hooks, one needs to select the each tag one by one and update the content inside it
+  each time when value gets updated, so here react hooks came to into picture, it will reflect the state updated value in UI
+  just call the function when you want to change the state value and it will handle the updation of state in UI
+*/
+
+function App() {
+  // let counter = 5;
+
+  let [counter, setCounter] = useState(5);
+
+  function addVal() {
+    // It will not go beyond 20
+    if(counter < 20) {
+      counter++;
+    }
+    setCounter(counter);
+  }
+
+  function remVal() {
+    // It will not go beyond 0
+    if(counter > 0) {
+      counter--;
+    }
+    setCounter(counter);
+  }
+
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addVal}>Add Value: {counter}</button>
+      <br/>
+      <button onClick={remVal}>Remove Value: {counter}</button>
+
+      <p>Para with Counter: {counter}</p>
+    </>
+  )
+}
+
+export default App
